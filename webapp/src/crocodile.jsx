ef64794effcc90c4addb1a4b1dec347639124e70
alex <onelove182123@gmail.com>
2025-12-26 18:45:15 +0300

    Краткое описание изменений

--- webapp/src/crocodile.jsx
+++ webapp/src/crocodile.jsx
@@ -821,7 +821,7 @@ function Setup({
     <AnimatePresence>
       {settingsOpen && (
         <motion.div
-          className="croco-settings-overlay"
+          className="croco-settings-overlay croco-settings-overlay--crocodile"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
@@ -829,7 +829,7 @@ function Setup({
           onClick={() => setSettingsOpen(false)}
         >
           <motion.div
-            className="croco-settings-window"
+            className="croco-settings-window croco-settings-window--crocodile"
             initial={{ y: 30, opacity: 0, scale: 0.98 }}
             animate={{ y: 0, opacity: 1, scale: 1 }}
             exit={{ y: 14, opacity: 0, scale: 0.98 }}
