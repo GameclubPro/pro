24367f6a475bb19d49e787326e3bf9f44dd5308e
Eka <onelove182123@gmail.com>
2026-01-02 23:00:43 +0300

    Add misha mini app and bot

---
+++ misha-app/src/types/telegram.d.ts
@@ -0,0 +1,16 @@
+export {};
+
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp: {
+        initData: string;
+        initDataUnsafe: any;
+        ready: () => void;
+        expand: () => void;
+        close: () => void;
+        sendData: (data: string) => void;
+      };
+    };
+  }
+}
